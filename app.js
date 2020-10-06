eeae0f7a8e6c5198c254ac62186a53b41def5fd1
Srikana Pal <pal.srikana@gmail.com>
2020-10-06 10:20:24 -0400

    Added Post

--- app.js
+++ app.js
@@ -29,7 +29,24 @@ app.get('/api/v1/tours', (req,res) => {
     });
 });
 
-//app.post('/api/v1/tours', (req,res))
+app.post('/api/v1/tours', (req,res) =>{
+    //console.log(req.body);
+    const newID = tours[tours.length -1].id+1;
+    const newTour = Object.assign( {id:newID}, req.body);
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours),err =>{
+        res.status(201).json({
+            status:'success',
+            data:{
+                tour:newTour
+            }
+        });
+    }
+    );
+
+    //res.send('Done');
+});
 
 const users = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/users.json`)
