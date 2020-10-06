705f816b1b88f0638506507e158a58d3117fe351
Srikana Pal <pal.srikana@gmail.com>
2020-10-06 09:57:54 -0400

    Modified app

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const express = require('express');
+const { Server } = require('http');
 
 const app = express();
 app.use(express.json());
@@ -28,11 +29,39 @@ app.get('/api/v1/tours', (req,res) => {
     });
 });
 
+//app.post('/api/v1/tours', (req,res))
 
+const users = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/users.json`)
+);
+
+app.get('/api/v1/users', (req,res) => {
+    res.status(200).json({
+        status : 'success',
+        result: users.length,
+        data:{
+            users
+        }
+    });
+});
+
+//const reviews = JSON.parse(
+//    fs.readFileSync(`${__dirname}/dev-data/data/reviews.json`)
+//);
+
+//app.get('/api/v1/reviews', (req,res) => {
+//    res.status(200).json({
+//        status : 'success',
+//        result: reviews.length,
+//        data:{
+//            reviews
+//        }
+//    });
+//});
 
 const port = 3000;
 
 
 app.listen(port, () => {
-    console.log('App running on port {port}..');
+    console.log('App running on port '+ port+ '..');
 });
