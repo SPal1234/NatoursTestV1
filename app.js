d7820445871b714e3a2ed01545bedbfd7cd5647b
Srikana Pal <pal.srikana@gmail.com>
2020-10-08 11:44:52 -0400

    Refactoring routes

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
 const fs = require('fs');
 const express = require('express');
 const { Server } = require('http');
+const { Router } = require('express');
 
 const app = express();
 app.use(express.json());
-
-//app.get('/', (req,res) =>{
-//    res
-//    .status(200)
-//    .json({message : 'Hello from the server!', app :'natours'} );
-//});
-
-//app.post('/', (req,res) =>{
-//    res.send('You can post to this endpoint');
-//});
 
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req,res) => {
+const getAllTours = (req,res) => {
     res.status(200).json({
         status : 'success',
         result: tours.length,
@@ -27,12 +18,11 @@ app.get('/api/v1/tours', (req,res) => {
             tours
         }
     });
-});
+};
 
-app.get('/api/v1/tours/:id', (req,res) => {
+const getTourById = (req,res) => {
 
     console.log(req.params);
-
     const id = req.params.id*1;
 
     if(id > tours.length) {
@@ -41,7 +31,6 @@ app.get('/api/v1/tours/:id', (req,res) => {
             message: 'Invalid ID'
         });
     }
-
     const tour = tours.find(el=>el.id === id);
     res.status(200).json({
         status : 'success',
@@ -50,32 +39,60 @@ app.get('/api/v1/tours/:id', (req,res) => {
             tour
         }
     });
-});
+};
 
-app.post('/api/v1/tours', (req,res) =>{
+const createTour = (req,res) =>{
     //console.log(req.body);
     const newID = tours[tours.length -1].id+1;
     const newTour = Object.assign( {id:newID}, req.body);
 
     tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours),err =>{
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours),_err =>{
         res.status(201).json({
             status:'success',
             data:{
                 tour:newTour
             }
         });
-    }
-    );
+    });
+};
 
-    //res.send('Done');
-});
+const updateTour = (req, res) => {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: '<Updated tour here...>'
+      }
+    });
+};
+
+const deleteTour = (req, res) => {
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  };
+
+//Read all Tours
+app.get('/api/v1/tours', getAllTours);
+
+//Find Tour by id
+app.get('/api/v1/tours/:id', getTourById);
+
+//Create Tour
+app.post('/api/v1/tours', createTour);
+
+//Update Tour by id search
+app.patch('/api/v1/tours/:id', updateTour);
+
+//Delete Tour by id
+app.delete('/api/v1/tours/:id', deleteTour);
 
 const users = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/users.json`)
 );
 
-app.get('/api/v1/users', (req,res) => {
+app.get('/api/v1/users', (_req,res) => {
     res.status(200).json({
         status : 'success',
         result: users.length,
@@ -103,5 +120,5 @@ const port = 3000;
 
 
 app.listen(port, () => {
-    console.log('App running on port '+ port+ '..');
+    console.log('App running on port '+ port+ '..')
 });
